ad45d60a3745f01789acf2a94416b6164eed7635
Ma Cat Huynh <uynhhuc810@gmail.com>
2024-11-06 03:28:26 +0700

    update controllers and index.ejs a bit

--- apps/home/indexController.js
+++ apps/home/indexController.js
@@ -5,12 +5,6 @@ async function renderHomePage(req, res) {
 	try {
 		const productsObject =
 			await indexService.getAllProductsAsObject();
-		// console.log(productsObject.computers);
-		// console.log(productsObject.mobilephones);
-		// console.log(productsObject.computers);
-
-		console.log(productsObject.televisions);
-
 		res.render("index", { products: productsObject });
 	} catch (error) {
 		console.error("Error rendering home page:", error);
