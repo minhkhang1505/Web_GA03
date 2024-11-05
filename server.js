bffd7a8961235bc561ba89452336c546f81c6401
Nguyen Minh Khang <nguyenminhkhang@MacBook-Air-cua-Nguyen-3.local>
2024-11-05 16:46:43 +0700

    update product list

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Pool } = require("pg");
 const app = express();
 const testRoute = require("./routes/testRoute");
 require("dotenv").config();
@@ -10,6 +11,43 @@ app.set("views", "views");
 // Middleware to serve static files
 app.use(express.static("public"));
 
+const pool = new Pool({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+app.get("/computers", async (req, res) => {
+	try {
+	  const result = await pool.query("SELECT * FROM Computers");
+	  res.render("category", { products: result.rows });
+	} catch (err) {
+	  console.error(err);
+	  res.send("Error " + err);
+	}
+});
+
+app.get("/category", async (req, res) => {
+	try {
+	  const result = await pool.query("SELECT * FROM MobilePhones");
+	  res.render("category", { products: result.rows });
+	} catch (err) {
+	  console.error(err);
+	  res.send("Error " + err);
+	}
+});
+
+app.get("/televisions", async (req, res) => {
+	try {
+	  const result = await pool.query("SELECT * FROM Televisions");
+	  res.render("category", { products: result.rows });
+	} catch (err) {
+	  console.error(err);
+	  res.send("Error " + err);
+	}
+});
 // Use the user routes
 app.use("/", testRoute);
 
@@ -17,5 +55,5 @@ app.use("/", testRoute);
 const HOST = process.env.SERVER_HOST || "localhost";
 const PORT = process.env.SERVER_PORT || 3000;
 app.listen(PORT, HOST, () => {
-	console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
