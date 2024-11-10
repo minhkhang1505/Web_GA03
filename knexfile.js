8d59362fcfa43d7c404e85abe06d4b35ca929c31
Nguyen Minh Khang <nguyenminhkhang@MacBook-Air-cua-Nguyen-5.local>
2024-11-10 18:17:31 +0700

    fix

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,3 @@
-const { connect } = require("./apps/home/indexRouter");
-
 require("dotenv").config();
 // Update with your config settings.
 
@@ -7,40 +5,19 @@ require("dotenv").config();
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-	development: {
-		client: "postgresql",
-		connection: {
-			host: `${process.env.DB_HOST}`,
-			user: `${process.env.DB_USERNAME}`,
-			password: `${process.env.DB_PASSWORD}`,
-			database: `${process.env.DB_NAME}`,
-			port: `${process.env.DB_PORT}`,
-			charset: "utf8",
-		},
+  development: {
+    client: "postgresql",
+    connection: {
+      host: `${process.env.DB_HOST}`,
+      user: `${process.env.DB_USERNAME}`,
+      password: `${process.env.DB_PASSWORD}`,
+      database: `${process.env.DB_NAME}`,
+      port: `${process.env.DB_PORT}`,
+      charset: "utf8",
+    },
 
-		migrations: {
-			directory: "./migrations",
-		},
-
-		seeds: {
-			directory: "./seeds",
-		},
-	},
-	production:{
-		client: "postgresql",
-		connection: {
-			connectionString: process.env.DB_CONNECTION_STRING,
-			ssl: {
-				rejectUnauthorized: false,
-			},
-			charset: "utf8",
-		},
-		
-		migrations: {
-			directory: "./migrations",
-		},
-		seeds: {
-			directory: "./seeds",
-		},
-	}
+    migrations: {
+      directory: "./migrations",
+    },
+  },
 };
--- server.js
+++ server.js
@@ -1,5 +1,40 @@
-const express = require("express");
+// const express = require("express");
+// const { Pool } = require("pg");
+// require("dotenv").config();
 
+// const indexRouter = require("./apps/home/indexRouter");
+// const televisionRouter = require("./apps/television/televisionRouter");
+// const mobilephoneRouter = require("./apps/mobilephone/mobilephoneRouter");
+// const computerRouter = require("./apps/computer/computerRouter");
+// const registrationRouter = require("./apps/registration/registrationRouter");
+
+// const pool = new Pool({
+//   connectionString: process.env.POSTGRES_URL,
+// });
+
+// const app = express();
+// // Set the view engine to EJS
+// app.set("view engine", "ejs");
+// app.set("views", "views");
+
+// // Middleware
+// app.use(express.static("public"));
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use("/", indexRouter);
+// app.use("/televisions", televisionRouter);
+// app.use("/mobilephones", mobilephoneRouter);
+// app.use("/computers", computerRouter);
+// app.use("/register", registrationRouter);
+
+// const HOST = process.env.SERVER_HOST || "localhost";
+// const PORT = process.env.SERVER_PORT || 3000;
+// app.listen(PORT, HOST, () => {
+//   console.log(`Server is running at http://${HOST}:${PORT}`);
+// });
+
+const express = require("express");
+const { Pool } = require("pg");
 require("dotenv").config();
 
 const indexRouter = require("./apps/home/indexRouter");
@@ -7,6 +42,10 @@ const televisionRouter = require("./apps/television/televisionRouter");
 const mobilephoneRouter = require("./apps/mobilephone/mobilephoneRouter");
 const computerRouter = require("./apps/computer/computerRouter");
 const registrationRouter = require("./apps/registration/registrationRouter");
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
 
 const app = express();
 // Set the view engine to EJS
@@ -21,9 +60,11 @@ app.use("/", indexRouter);
 app.use("/televisions", televisionRouter);
 app.use("/mobilephones", mobilephoneRouter);
 app.use("/computers", computerRouter);
-app.use("/register",registrationRouter);
+app.use("/register", registrationRouter);
 
-const PORT = process.env.SERVER_PORT || 3000;
-app.listen(PORT, () => {
-	console.log(`Server is running on port: ${PORT}`);
+// Start the server
+const HOST = process.env.SERVER_HOST || "localhost";
+const PORT = process.env.SERVER_PORT || 3001;
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
